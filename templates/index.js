78342dde7e9ee67f5ade4de7e09d8a81d7a9d414
chiptus <chiptus@gmail.com>
2017-09-14 10:45:54 -0700

    add stateful component template

--- templates/index.js
+++ templates/index.js
@@ -12,6 +12,28 @@ const {{pascalCase}} = () => (
 export default {{pascalCase}};
 `;
 
+const statefulComponent = `import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import './{{paramCase}}.css';
+
+class {{pascalCase}} extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (<div className="{{paramCase}}"></div>);
+  }
+}
+
+{{pascalCase}}.propTypes = {}
+
+export default {{pascalCase}};
+`;
+
 const index = `import {{pascalCase}} from './{{paramCase}}.js'
 export default {{pascalCase}};
 `;
@@ -20,4 +42,4 @@ const style = `.{{paramCase}} {
   
 }`;
 
-module.exports = { index, style, component };
+module.exports = { index, style, component, statefulComponent };
